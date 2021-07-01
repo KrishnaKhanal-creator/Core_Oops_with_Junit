54e3b86cea6e1d1b23341434bf36813a6c203c0d
krishnaKhanal-creator <krishna.khanal2@outlook.com>
2021-07-01 12:48:14 -0500

    small fix

--- src/main/java/com/kk/oppsproject/hrsystem/client/MainTester.java
+++ src/main/java/com/kk/oppsproject/hrsystem/client/MainTester.java
@@ -7,9 +7,11 @@ public class MainTester {
 		try {
 			TransactionPicker.pickTransaction();
 		}catch (NullPointerException e) {
-			e.getMessage();
+			e.printStackTrace();
+			System.out.println("No data found of your entry");
 			TransactionPicker.pickTransaction();
 		}catch (InputMismatchException e){
+			e.printStackTrace();
 			System.out.println("You Entered unwanted type of input!!\n Please enter carefully with this new transaction now!");
 			TransactionPicker.pickTransaction();
 		}
